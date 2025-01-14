0c9a031f11f7118669fa26333dcb49cc8d1f7e28
Susmit Bahadkar <121481904+bobbybacala@users.noreply.github.com>
2025-01-14 23:28:15 +0530

    axios instance

--- client/src/libs/axios.js
+++ client/src/libs/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const axiosInstance = axios.create({
     // if we are in development mode, we want to use localhost else the domain name
-    baseURL: import.meta.mode === 'development' ? 'http://localhost:3000/api/v1' : '/api/v1',
+    baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:3000/api/v1' : '/api/v1',
     withCredentials: true // for cookies with credentials, this tells the backend that we are authenticated
 })
 
